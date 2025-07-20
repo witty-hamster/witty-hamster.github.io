8786af387326fac0a41530335097747d58c97a3f
hamster <cuiguodong_2012@163.com>
2025-07-21 00:23:32 +0800

    取消多语言环境

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,20 +3,22 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  // base: "/",
 
-  locales: {
-    "/": {
-      lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
-    },
-  },
+  // locales: {
+  //   "/": {
+  //     lang: "zh-CN",
+  //     title: "博客演示",
+  //     description: "vuepress-theme-hope 的博客演示",
+  //   },
+  //   "/en/": {
+  //     lang: "en-US",
+  //     title: "Blog Demo",
+  //     description: "A blog demo for vuepress-theme-hope",
+  //   }
+  // },
+
+  lang: "zh-CN",
 
   theme,
 
